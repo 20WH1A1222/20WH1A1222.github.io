2c5d5527c833bce14310e715b01175647796bcc8
20WH1A1222 <97152848+20WH1A1222@users.noreply.github.com>
2023-06-13 22:19:30 +0530

    save

--- usercard3.js
+++ usercard3.js
@@ -10,8 +10,14 @@ let displayobject;
 let getRandomUser=function(){
     fetch("https://randomuser.me/api")
     .then(response => response.json())
-    .then(data =>console.log(data))
-    document.getElementById("cat-img").src=displayobject.imgurl;
-    document.getElementById("cat-name").innerHTML=displayobject.name;
-    document.getElementById("para").innerHTML=displayobject.Description;
+    .then(data =>{
+        displayobject.name = data.results[0].name.first+" "+data.results[0].name.last
+        displayobject.imageurl=data.results[0].picture.large
+        displayobject.description=data.results[0].gender
+        document.getElementById("cat-img").src=displayobject.imgurl;
+        document.getElementById("cat-name").innerHTML=displayobject.name;
+        document.getElementById("para").innerHTML=displayobject.Description;
+      
+    })
+    
 }
